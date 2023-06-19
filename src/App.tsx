4949d4b06736d1a1906bd11262c90ba271dc44bb
denis-ttk-1975 <denis.ttk@gmail.com>
2023-06-19 13:44:04 +0300

    forwardRef also added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,4 @@
-import React, { useState, useRef } from 'react';
-
-const InputWithRef = () => {
-  const [valueRefInput, setRefValue] = useState('');
-  const inputValueRef = useRef<HTMLInputElement>(null);
-
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // funcToInterceptInputValue(e.target.value);
-    if (inputValueRef.current) {
-      setRefValue(inputValueRef.current.value);
-    }
-  };
-
-  return (
-    <>
-      <input type='text' ref={inputValueRef} onChange={onChangeInput} name='value' />
-      <div>inputValueRef = {inputValueRef.current ? inputValueRef.current.value : 'значение не определено'}</div>
-      <div>valueRefInput = {valueRefInput}</div>
-    </>
-  );
-};
+import React, { useState, useRef, forwardRef } from 'react';
 
 const Input = () => {
   const [valueInput, setValue] = useState('');
@@ -30,6 +10,25 @@ const Input = () => {
     <>
       <input value={valueInput} onChange={onChangeInput} name='value' />
       <div>valueInput = {valueInput}</div>
+    </>
+  );
+};
+
+const InputWithRef = () => {
+  const [valueRefInput, setRefValue] = useState('');
+  const inputValueRef = useRef<HTMLInputElement>(null);
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (inputValueRef.current) {
+      setRefValue(inputValueRef.current.value);
+    }
+  };
+
+  return (
+    <>
+      <input type='text' ref={inputValueRef} onChange={onChangeInput} name='value' />
+      <div>inputValueRef = {inputValueRef.current ? inputValueRef.current.value : 'значение не определено'}</div>
+      <div>valueRefInput = {valueRefInput}</div>
     </>
   );
 };
@@ -46,14 +45,37 @@ const InputWithExternalState = (props: { valueInput: string; externalSetState: R
   );
 };
 
+const InputWithForwardRef = forwardRef((props: { valueInput: string; externalSetState: React.Dispatch<React.SetStateAction<string>> }, ref: React.LegacyRef<HTMLInputElement> | undefined) => {
+  // const [valueRefForwardInput, setRefForwardValue] = useState('');
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (ref != null && typeof ref !== 'function' && typeof ref !== 'string' && ref.current != null) {
+      console.log(1);
+      props.externalSetState(ref.current.value);
+    }
+  };
+
+  return (
+    <>
+      <input type='text' ref={ref} onChange={onChangeInput} name='value' />
+      <div>inputValueRef = {ref != null && typeof ref !== 'function' && typeof ref !== 'string' && ref.current != null ? ref.current.value : 'значение не определено'}</div>
+      <div>valueRefForwardInput = {props.valueInput}</div>
+    </>
+  );
+});
+
 export const App = () => {
   const [valueInput3, setValue3] = useState('');
+  const refForwardValue = useRef(null);
+  const [valueInput4, setValue4] = useState('');
   return (
     <div className='App'>
       <Input />
       <InputWithRef />
       <InputWithExternalState externalSetState={setValue3} valueInput={valueInput3} />
       <div>valueInputWithExternalState = {valueInput3}</div>
+      <InputWithForwardRef externalSetState={setValue4} valueInput={valueInput4} ref={refForwardValue} />
+      <div>valueInputWithForwardRef = {valueInput4}</div>
     </div>
   );
 };
